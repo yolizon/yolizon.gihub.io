e5defdd21eb3ed3fb8af5fc9f05f90b8ce87e4b0
yolizon <75636853+yolizon@users.noreply.github.com>
2021-01-10 16:36:43 +0200

    changed old files and add new

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// comment
+'use strict';
+
+/* let a = +(prompt('enter a:',''));
+let b = +(prompt('enter b:',''));
+if(isNaN(a) && isNaN(b)) {
+    alert('a or b not a number');
+}
+let o =prompt('enter operation','');
+if(o=='+') {
+    alert(a+b);
+}
+else if(o=='-'){
+    alert(a-b);
+}
+else if(o=='/'){
+    alert(a-b);
+}
+else if(o=='*'){
+    alert(a*b);
+}
+else if(o=='%'){
+    alert(a%b);
+}
+else {
+    alert('wrong operation');
+} */
+let key = 'value2';
+switch (key) {
+    case 'value1':
+        alert('Hello 1')
+        break;
+        case 'value2':
+            alert('Hello 2')
+            break;
+
+    default:
+        alert('G B')
+        break;
+}
