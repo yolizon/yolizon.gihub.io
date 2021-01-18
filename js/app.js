621f133add80c493ae851c6bd93c1e838afb3aef
yolizon <75636853+yolizon@users.noreply.github.com>
2021-01-18 15:53:24 +0200

    add files via uppload

--- js/app.js
+++ js/app.js
@@ -1,43 +1,94 @@
 // comment
 'use strict';
-// document.body.innerHTML ="<h1>Helloooo</h1>";
-// document.body.style.backgroundColor = "green";
-const shoppingCart=document.getElementById('shopping-cart');
-const aside=document.querySelector('.aside');
-const closeBtn=document.querySelector('.close-btn');
-
-let toggleCart=function(){
-    aside.classList.toggle('show-sidebar');
-   
-};
-shoppingCart.addEventListener('click', toggleCart);
-closeBtn.addEventListener('click', function(){
-    aside.classList.remove('show-sidebar');
-
-})
-
-let productDetails = document.querySelectorAll('.product-detail');
-
-const single = document.querySelector('.single');
+const aside = document.querySelector(".aside");
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const clearCart = document.querySelector(".clear-cart");
+const single = document.querySelector(".single");
 const goBack = document.querySelector('.go-back');
-productDetails.forEach(function(element){
-  element.addEventListener('click',function(e){
-    //console.dir(e.target.closest('.product'));
-    let parent = e.target.closest('.product');
-    let name = parent.querySelector('.product-name').innerText;
-    let price = parent.querySelector('.product-price').innerText;
-    let image = parent.querySelector('.product-img').getAttribute('src');
 
-    //console.log(name,price);
-    document.querySelector('.product_img').innerHTML =`<img scr="${image}" alt="${name}"`;
-    single.classList.add('show-single');
-    document.querySelector('.description_content--about').innerHTML =`
-    <h1>${name}</h1>
-    <h2>${price}</h2>
-    <p> цветочная композиция</p>
-    `;
+function toggleCart() {
+    if(single.classList.contains('show-single')){
+        single.classList.remove('show-single');
+    }
+    aside.classList.toggle("show");
+}
+
+function closeCart() {
+    aside.classList.remove("show");
+}
+
+function createProduct(data){
+  return `
+  <div class="col-xl-3 col-lg-4 col-sm-6">
+    <div class="product text-center">
+      <div class="position-relative mb-3">
+          <a href="#" class="d-block product-detail">
+              <img  class="img-fluid w-100 product-img" src="${data.image}" alt="${data.image}">
+          </a>    
+          <div class="product-overlay">
+              <ul class="list-inline">
+                  <li class="list-inline-item">
+                      <a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i></a>
+                  </li>
+                  <li class="list-inline-item">
+                      <a href="#" class="btn btn-sm btn-dark add-to-cart"><i class="fas fa-cart-plus"></i> Add to cart</a>
+                  </li>
+                  <li class="list-inline-item">
+                      <a class="btn btn-sm btn-outline-dark product-detail" href="#"><i class="fas fa-expand"></i></a>
+                  </li>
+              </ul>
+          </div>
+      </div>        
+
+      <h3 class="reset-anchor product-name">${data.name}</h3>
+      <p class="text-small text-muted">$<span class="product-price">${data.price}</span></p>
+    </div>
+  </div>
+  `;
+}
+function makeShowcase(products){
+  let result = '';
+
+  products.forEach(function(element){
+    result += createProduct(element);
+
+  });
+  document.querySelector('.showcase').innerHTML = result;
+}
+function renderShowcase(){
+  let addToCarts = document.querySelectorAll('.add-to-cart');
+  let productDetails = document.querySelectorAll('.product-detail');
+
+  goBack.addEventListener('click',function(){
+    single.classList.remove('show-single');
   })
-})
-goBack.addEventListener('click',function(){
-  single.classList.remove('show-single');
-})
+  productDetails.forEach(function(element){
+    element.addEventListener('click',function(e){
+      //console.dir(e.target.closest('.product'));
+      let parent = e.target.closest('.product');
+      let name = parent.querySelector('.product-name').innerText;
+      let price = parent.querySelector('.product-price').innerText;
+      let about = `
+      <h1>${name}</h1>
+        <h2>$${price}</h2>
+        <div>цветочная композиция</div>
+      `;
+      //description__content--about
+      let contentAbout = document.querySelector('.description_content--about');
+      contentAbout.innerHTML = about;
+
+      //product-img
+      let image = parent.querySelector('.product-img').getAttribute('src');
+      document.querySelector('.product_img').innerHTML = `<img src="${image}" alt="">`;
+      single.classList.add("show-single");
+    });
+  })
+}
+document.addEventListener("DOMContentLoaded", function(){
+  closeBtn.addEventListener("click", closeCart);
+  sidebarToggle.addEventListener("click", toggleCart);
+
+  makeShowcase(products);
+  renderShowcase();
+});
---
+++ js/product.js
@@ -0,0 +1,39 @@
+const products = [
+
+{
+    name: "Test product",
+    price: 250,
+    image: "images/свадбукет2.jpg",
+    description:"цветочная композиция"
+},
+{
+    name: "Test product",
+    price: 250,
+    image: "images/композиция (2).jpg" ,
+    description:"цветочная композиция"
+},
+{
+    name: "Test product",
+    price: 250,
+    image: "images/мастеркласс.jpg",
+    description:"цветочная композиция"
+},
+{
+    name: "Test product",
+    price: 250,
+    image: "images/кафе.jpg",
+    description:"цветочная композиция"
+},
+{
+    name: "Test product",
+    price: 250,
+    image: "images/озеленение.jpg",
+    description:"цветочная композиция"
+},
+{
+    name: "Test product" ,
+    price: 250 ,
+    image: "images/озеленение.jpg",
+    description:"цветочная композиция"
+}
+];
